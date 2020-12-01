170502549efa0a068fca041a107f87028712df76
khang28032002 <51906688+khang28032002@users.noreply.github.com>
2020-12-01 19:15:05 +0700

    Add files via upload
    add more cases that bot can win

--- Tic - Tac - Toe.cpp
+++ Tic - Tac - Toe.cpp
@@ -1,94 +1,86 @@
-// Tic - Tac - Toe v1.0
-//It still have many failed testcases and a lot to improve
+// Tic - Tac - Toe v2.0
+//It still have many failed test cases and a lot to improve
 //Graphic and sound is still in progress, AI for 1 player will be add in the near future
 
 #include <iostream>
 #include "GameFunction.h"
 #include <vector>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    vector<char> square;
-    char c = '1';
-    int player = 1;
-    char choice = '0';
-    int i = -1;
-    int check;
-    for (int i = 0; i < 9; i++)                         //input element into the vector
+    cout << "Welcome to Tic Tac Toe game \nThis is my first Tic Tac Toe game\n\n";     //instruction
+    int size, mode=0;
+    cout << "Choose game mode\n";
+    cout << "1: VS Computer----------2: Two Players\n";
+    cin >> mode;
+    int checkMode= ((mode == 1) || (mode == 2)) ? true : false;
+    while (!checkMode)
     {
-        square.push_back(c);
-        c = (int)c + 1;
-        c = (char)c;
+        cout << "Please choose again ";
+        cin >> mode;
+        checkMode = ((mode == 1) || (mode == 2)) ? true : false;
     }
-    board(square);                                      //render the board 
-    do
+    cout << "Input the board size ";
+    cin >> size;
+    while (size <= 2)
     {
-        cout << "Welcome to Tic Tac Toe game \nThis is my first Tic Tac Toe game\n\n";
-        cout << "Player 1 is X-------Player 2 is O\n" ;                 //instruction
-        cout << "Input the tile number to make your move \n"<<endl;
-        cout << "Player " << player << " turn " << endl;
-        cin >> choice;                                          //input the tile number
-        check = checkFalseInput(&player, &choice, square);
-        while (check != 1)
+        cout << "Please choose again ";
+        cin >> size;
+    }
+    int  player = 1;
+    char board[100][100],pSign='X',cSign='O';
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
         {
-            cout << "Please choose a different tile " << endl;
-            cin >> choice;
-            check = checkFalseInput(&player, &choice, square);
+            board[i][j] =' ';
         }
-        playerInput(player, choice, square);                    //check input and make the move
-        system("CLS");                                          //clear screen
-        board(square);                                          //update the screen
-        i=checkwin(square);                                     //check winner
-        if (i == 1)
+    }
+    system("CLS");
+    displayBoard(board,size);
+    if (mode == 2)
+    {
+        modePVP(player, size, board);
+    }
+    else
+    {
+        do
         {
-            cout << "Player " << player << " win ";
-            return 0;
-        }
-        else if (i == 0)
-        {
-            cout << "Draw";
-            return 0;
-        }
-        player+=1;                                              //change to next player
-        cout << "Welcome to Tic Tac Toe game \nThis is my first Tic Tac Toe game\n\n";
-        cout << "Player 1 is X-------Player 2 is O\n";
-        cout << "Input the tile number to make your move \n"<<endl;
-        cout << "Player " << player << " turn " << endl;
-        cin >> choice;
-        check = checkFalseInput(&player, &choice, square);
-        while (check != 1)
-        {
-            cout << "Please choose a different tile " << endl;
-            cin >> choice;
-            check = checkFalseInput(&player, &choice, square);
-        }
-        playerInput(player, choice, square);
-        system("CLS");
-        board(square);
-        i = checkwin(square);
-        if (i == 1)
-        {
-            cout << "Player " << player << " win ";
-            return 0;
-        }
-        else if (i == 0)
-        {
-            cout << "Draw";
-            return 0;
-        }
-        player-=1;
+            if (gameUpdate(board, size, player) != -1) return 0;
+            system("CLS");
+            if (botMove(size, board,cSign) == -1)           //if the bot is about to win, make that move
+            {
+                if (botMove(size,board,pSign) == -1)        //if the player is about to win, stop the player
+                {
+                    botRandom(size, board);                 //if not the above option then random the move
+                    displayBoard(board, size);
+                }
+                else
+                {
+                    displayBoard(board, size);
+                    continue;
+                }
 
-    } while (i == -1);
+            }
+            else
+            {
+                displayBoard(board, size);
+                cout << "Computer win";
+                return 0;
+            }
+        } while (true);
+    }
     return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
+// Tips for Getting Started:
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
